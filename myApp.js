efbbe02d1130b650f97a5a509088e2ac36ab472c
Maxim Shnitko <MaximShnitko@gmail.com>
2023-05-31 15:54:56 +0300

    Fixes

--- myApp.js
+++ myApp.js
@@ -4,12 +4,13 @@ console.log("Hello World!");
 
 absolutePath = __dirname + '/views/index.html';
 
-app.use("/public", express.static( __dirname + "/public"));
-
-app.use((req, res, next) =>{
-  console.log(req.method + " " + req.path + " " + req.ip);
+app.use(function(req, res, next) {
+  console.log(req.method, req.path, req.ip)
   next();
 })
+
+app.use("/public", express.static( __dirname + "/public"));
+
 
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
