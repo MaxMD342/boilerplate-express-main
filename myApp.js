8c6ccaf8d6c68a318478a548376a5bb0f39dde32
Maxim Shnitko <MaximShnitko@gmail.com>
2023-05-31 08:40:16 +0300

    Added: app.use(express.static(__dirname + '/public'));

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ console.log("Hello World!");
 
 absolutePath = __dirname + '/views/index.html';
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 });
